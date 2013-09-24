e4e1f1375271c55e96e7974726448bf0bdab389a
Jan Dantes <paglubogngaraw@gmail.com>
2013-09-25 05:15:36 +0800

    Removed handler for Notes

--- RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/AddRecipe.java
+++ RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/AddRecipe.java
@@ -12,8 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddRecipe extends Activity implements DialogInterface.OnClickListener {
-    //private DatabaseHelper mDatabaseHelper;
-    private EditText recipeName, recipeUrl, recipeNotes;
+    private EditText recipeName, recipeUrl;
 
     private DatabaseHelper rec;
 
@@ -22,12 +21,9 @@ public class AddRecipe extends Activity implements DialogInterface.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addrecipe);
         setTitle("Add New Recipe");
-        //mDatabaseHelper = new DatabaseHelper(this);
-
 
         recipeName =  (EditText)findViewById(R.id.txt_recipeName);
         recipeUrl = (EditText)findViewById(R.id.txt_recipeUrl);
-        recipeNotes = (EditText)findViewById(R.id.txt_recipeNotes);
         final Button addNewRecipe = (Button)findViewById(R.id.btn_saveButton);
 
         addNewRecipe.setOnClickListener(new View.OnClickListener() {
@@ -35,13 +31,12 @@ public class AddRecipe extends Activity implements DialogInterface.OnClickListen
             public void onClick(View v) {
                 String nameText = recipeName.getText().toString();
                 String urlText = recipeUrl.getText().toString();
-                String notesText = recipeNotes.getText().toString();
-                addRecipe(nameText,urlText ,notesText);
+                addRecipe(nameText,urlText);
             }
         });
     }
 
-    private void addRecipe(String recipeName,String recipeUrl, String recipeNotes){
+    private void addRecipe(String recipeName,String recipeUrl){
         rec = new DatabaseHelper(this);
         ContentValues values = new ContentValues();
         if(recipeName != null){
@@ -50,7 +45,6 @@ public class AddRecipe extends Activity implements DialogInterface.OnClickListen
         if(recipeUrl != null){
             values.put(rec.COL_RECIPE_URL, recipeUrl);
         }
-        values.put(rec.COL_RECIPE_NOTES, recipeNotes);
         try {
             rec.insert(DatabaseHelper.TABLE_RECIPES, values);
             Intent loadRecipeList = new Intent(AddRecipe.this, RecipeList.class);
@@ -63,7 +57,6 @@ public class AddRecipe extends Activity implements DialogInterface.OnClickListen
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.add_recipe, menu);
         return true;
     }
