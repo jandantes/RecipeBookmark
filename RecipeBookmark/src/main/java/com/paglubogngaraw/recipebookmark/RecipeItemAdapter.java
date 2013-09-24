e0b7b26c848eb07d04579fca17fb6f6ac20bb1ec
Jan Dantes <paglubogngaraw@gmail.com>
2013-09-25 05:21:58 +0800

    Finally working code for custom SimpleCursorAdapter

--- RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/RecipeItemAdapter.java
+++ RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/RecipeItemAdapter.java
@@ -1,18 +1,47 @@
 package com.paglubogngaraw.recipebookmark;
 
-import android.app.Activity;
-import android.text.Layout;
-import android.widget.BaseAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 /**
  * Created by jan.dantes on 9/25/13.
  */
-public class RecipeItemAdapter extends BaseAdapter{
-    private Activity activity;
-    private ArrayList<HashMap<String, String>> data;
-    private  static Layout
+public class RecipeItemAdapter extends SimpleCursorAdapter {
+    private Context mContext;
+    private int layout;
+    private Cursor cr;
+    private LayoutInflater inflater;
 
+    public RecipeItemAdapter(Context context,int layout, Cursor c,String[] from,int[] to)  {
+        super(context,layout,c,from,to,0);
+        this.layout = layout;
+        this.mContext = context;
+        this.inflater = LayoutInflater.from(context);
+        this.cr = c;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        TextView name = (TextView)view.findViewById(R.id.txt_recipeName);
+        TextView url = (TextView)view.findViewById(R.id.txt_recipeUrl);
+        TextView course = (TextView)view.findViewById(R.id.txt_recipeCourse);
+        TextView ingredient = (TextView)view.findViewById(R.id.txt_recipeIngredient);
+
+        name.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_RECIPE_NAME)));
+        url.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_RECIPE_URL)));
+        course.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_RECIPE_COURSE)));
+        ingredient.setText(cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_RECIPE_INGREDIENT)));
+
+    }
+    @Override
+    public View newView (Context context, Cursor cursor, ViewGroup parent) {
+        return inflater.inflate(layout, null);
+    }
 }
+
