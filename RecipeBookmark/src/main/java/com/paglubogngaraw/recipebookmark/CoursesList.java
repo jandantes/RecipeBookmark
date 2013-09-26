93eb00c3a6ca89414bdeb8518dd3fb9c7b694438
Jan Dantes <paglubogngaraw@gmail.com>
2013-09-27 03:28:56 +0800

    Fragment for Courses.

---
+++ RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/CoursesList.java
@@ -0,0 +1,38 @@
+package com.paglubogngaraw.recipebookmark;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by jan.dantes on 9/27/13.
+ */
+public class CoursesList extends Fragment {
+
+    private ListView courseList;
+    private ListViewItemAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_courses,container, false);
+        courseList = (ListView) rootView.findViewById(R.id.listView_courses);
+        return rootView;
+    }
+
+    public  void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        adapter = new ListViewItemAdapter(
+                getActivity(),
+                R.layout.listview_row_courses,
+                R.id.txt_courseName,
+                getResources().getStringArray(R.array.courses_array)
+        );
+        courseList.setAdapter(adapter);
+
+    }
+
+}
