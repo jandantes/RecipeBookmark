afee9d586566b59ef38d8d0ed49c78ef14c69d8f
Jan Dantes <paglubogngaraw@gmail.com>
2013-09-27 03:29:13 +0800

    Fragment for Ingredients.

---
+++ RecipeBookmark/src/main/java/com/paglubogngaraw/recipebookmark/IngredientsList.java
@@ -0,0 +1,38 @@
+package com.paglubogngaraw.recipebookmark;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by jan.dantes on 9/27/13.
+ */
+public class IngredientsList extends Fragment {
+
+    private ListView courseList;
+    private ListViewItemAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_ingredients,container, false);
+        courseList = (ListView) rootView.findViewById(R.id.listView_ingredients);
+        return rootView;
+    }
+
+    public  void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        adapter = new ListViewItemAdapter(
+                getActivity(),
+                R.layout.listview_row_ingredients,
+                R.id.txt_ingredientName,
+                getResources().getStringArray(R.array.ingredients_array)
+        );
+        courseList.setAdapter(adapter);
+
+    }
+
+}
